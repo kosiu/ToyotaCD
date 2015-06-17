bed2a6e300c5dcee9b336abfb5bba411a69d9253
kosiu <jacek.kosek@gmail.com>
2015-06-17 17:13:59 +0200

    avclandrv - cleaning in header

--- avclandrv.h
+++ avclandrv.h
@@ -30,37 +30,12 @@
 //------------------------------------------------------------------------------
 #include <inttypes.h>
 
-// Public Macro
+// Macros
+
 // true if message from some device on AVCLan begin
 #define INPUT_IS_SET (ACSR & _BV(ACO))
 
-/* Private macro */
-#define INPUT_IS_CLEAR (!(ACSR & _BV(ACO)))
-
-// Private timers interupt on off to be checked
-#define STOPEvent  cbi(TIMSK1, TOIE1); cbi(UCSR0B, RXCIE0);
-// Private timers interupt on off to be checked
-#define STARTEvent sbi(TIMSK1, TOIE1); sbi(UCSR0B, RXCIE0);
-
-// Not used might be useful
-#define CHECK_AVC_LINE	if (INPUT_IS_SET) AVCLan_Read_Message();
-
-// Private: block line eg. you can't read
-void AVC_HoldLine();
-// Private: (not used in main program)
-void AVC_ReleaseLine();
-
 #define MAXMSGLEN	32
-
-// Head Unid ID 1
-uint8_t HU_ID_1;		// 0x01
-// Head Unid ID 2
-uint8_t HU_ID_2;		// 0x40
-
-// Device ID 1
-uint8_t CD_ID_1;		// 0x03
-// Device ID 2
-uint8_t CD_ID_2;		// 0x60
 
 // Commands:
 #define cmNull		0
@@ -70,19 +45,19 @@ uint8_t CD_ID_2;		// 0x60
 #define cmStatus4	4
 
 // JKK public: used in sniffer.c
-#define cmRegister	100
-#define cmInit		101
-#define cmCheck		102
-#define cmPlayIt	103
-#define cmBeep		110
+#define cmRegister	100 //0x64
+#define cmInit		101 //0x65
+#define cmCheck		102 //0x66
+#define cmPlayIt	103 //0x67
+#define cmBeep		110 //0x6E
 
-#define cmNextTrack	120
-#define cmPrevTrack	121
-#define cmNextDisc	122
-#define cmPrevDisc	123
+#define cmNextTrack	120 //0x78
+#define cmPrevTrack	121 //0x79
+#define cmNextDisc	122 //0x7A
+#define cmPrevDisc	123 //0x7B
 
-#define cmScanModeOn	130
-#define cmScanModeOff	131
+#define cmScanModeOn	130 //0x82
+#define cmScanModeOff	131 //0x83
 
 #define cmPlayReq1	5
 #define cmPlayReq2	6
@@ -93,19 +68,32 @@ uint8_t CD_ID_2;		// 0x60
 typedef enum { stStop=0, stPlay=1 } cd_modes;
 cd_modes CD_Mode;
 
-// Place where whole message is stored during reading
-uint8_t broadcast;
-uint8_t master1;
-uint8_t master2;
-uint8_t slave1;
-uint8_t slave2;
-uint8_t message_len;
-uint8_t message[MAXMSGLEN];
+//-------------------------------------------------------------------------
+
+// Head Unid ID 1
+extern uint8_t HU_ID_1;		// 0x01
+// Head Unid ID 2
+extern uint8_t HU_ID_2;		// 0x40
+
+// Device ID 1
+extern uint8_t CD_ID_1;		// 0x03
+// Device ID 2
+extern uint8_t CD_ID_2;		// 0x60
+
+extern uint8_t cd_Disc;
+extern uint8_t cd_Track;
+extern uint8_t cd_Time_Min;
+extern uint8_t cd_Time_Sec;
+
+extern uint8_t playMode;
+
+// if answer requested
+extern uint8_t answerReq;
 
 
-uint8_t data_control;
-uint8_t data_len;
-uint8_t data[MAXMSGLEN];
+
+//------------------------------------------------------------------------------
+
 
 /* Public function AVCLan_Read_Message()
  Read the message, store it, process it, prepare answer.
@@ -131,7 +119,7 @@ void AVCLan_Send_Status();
 // Initialize AVCLan
 void AVCLan_Init();
 
-/* Private */
+/* Sends only register line */
 void AVCLan_Register();
 
 /*  Public function uint8_t AVCLan_SendData()
@@ -161,31 +149,14 @@ uint8_t  AVCLan_SendDataBroadcast();
 // Send command eg: cmBeep
 uint8_t  AVCLan_Command(uint8_t command);
 
-
+// Increment Toyota Type
 uint8_t  HexInc(uint8_t data);
-uint8_t  HexDec(uint8_t data);
-uint8_t  Dec2Toy(uint8_t data);
-
-uint8_t check_timeout;
-
-uint8_t cd_Disc; // JKK public: used in sniffer.c
-uint8_t cd_Track;
-uint8_t cd_Time_Min;
-uint8_t cd_Time_Sec;
-
-uint8_t playMode;
 
 // Public: Function send message
 uint8_t AVCLan_SendMyData(uint8_t *data_tmp, uint8_t s_len);
 // Public: Function send brodcast massage
 uint8_t AVCLan_SendMyDataBroadcast(uint8_t *data_tmp, uint8_t s_len);
 
-void ShowInMessage();
-void ShowOutMessage();
 
-//------------------------------------------------------------------------------
-
-// Public: if answer requested
-uint8_t answerReq;
 //------------------------------------------------------------------------------
 #endif
